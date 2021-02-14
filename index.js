fb7f677a87335b404444e8d0ec7db076326e8de2
Leopra <Leopica1>
2021-02-14 13:26:21 +0200

    3.16-3.18

--- index.js
+++ index.js
@@ -17,56 +17,26 @@ morgan.token('body', function (req, res) {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
-
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person.find({}).then(p => {
         response.json(p.map(p => p.toJSON()))
     })
+        .catch(error => next(error))
 })
 
-app.get('/api/info', (request, response) => {
-    //as i needed to get the count before sendind the response i had to make the promise sinchronous, there is a better way?
-    async function loadcount() {
-        let conto = undefined
-        try {
-            conto = await Person.count({}, function (err, count) {
-            })
-        }
-        catch (error) {
-            console.error('oops, something went wrong!', error);
-        }
-        const info = `Phonebook has info for ${conto}`
+app.get('/api/info', (request, response, next) => {
+    Person.count({}).then(c => {
+        const info = `Phonebook has info for ${c}`
         const time = new Date()
         response.setHeader('Content-type', 'text/html')
         response.send(`<div><div> ${info} people</div><div>${time}</div></div>`)
-    }
-    loadcount()
+    })
+        .catch(error => next(error))
+
 })
 
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     const person = Person.findById(request.params.id).then(p => {
         if (p) {
             response.json(p.toJSON())
@@ -76,30 +46,25 @@ app.get('/api/persons/:id', (request, response) => {
 
         }
     })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    Person.deleteOne({_id: request.params.id}).then(pers => {
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.deleteOne({ _id: request.params.id }).then(pers => {
         if (pers) { response.json(pers).end() } else {
             response.status(404).end()
         }
     }
-    ).catch(error => {
-        console.log(error)
-        response.status(500).end()
-    })
+    ).catch(error => next(error))
+
 })
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const newpers = request.body
 
     if (!newpers.name || !newpers.number) {
         response.status(404).send({ 'error': "missing name or number" })
-    }
-
-    else if (persons.find(pers => pers.name.toLowerCase() === newpers.name.toLowerCase())) {
-        response.status(404).send({ 'error': "person already existing" })
     }
 
     else {
@@ -111,14 +76,46 @@ app.post('/api/persons', (request, response) => {
         pers.save().then(saved => {
             response.json(saved.toJSON())
         })
+            .catch(error => next(error))
+
     }
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const pers = {
+        name: body.name,
+        number: body.number,
+        date: new Date()
+    }
+
+    Person.findByIdAndUpdate(request.params.id, pers, { new: true })
+        .then(newpers => {
+            response.json(newpers)
+        })
+        .catch(error => next(error))
+})
+
+
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
-  }
-  
+}
+
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 
